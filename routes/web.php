8e78c1a74f00fea6a54acbff5116249b6fed8506
Diana-LRG <98989631+Diana-LRG@users.noreply.github.com>
2022-05-03 01:15:44 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CasillaController;
+use App\Http\Controllers\CandidatoController;
+use App\Http\Controllers\VotoController;
+use App\Http\Controllers\EleccionController;
+//use use App\Http\Controllers\CustomAutchController;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('casilla', CasillaController::class);
+Route::resource('candidato', CandidatoController::class);
+Route::resource('voto', VotoController::class);
+Route::resource('eleccion', EleccionController::class);
+
